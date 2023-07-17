05b7e3306229c424748d5249d024ce072b1f13e9
Lev Blit <levblt1@gmail.com>
2023-07-17 21:55:56 +0300

    solutions

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -5,12 +5,15 @@
 // Execute `rustlings hint enums3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
+#[derive(Debug)]
 enum Message {
-    // TODO: implement the message variant types based on their usage below
+    ChangeColor(u8, u8, u8),
+    Echo(String),
+    Move(Point),
+    Quit,
 }
 
+#[derive(Debug)]
 struct Point {
     x: u8,
     y: u8,
@@ -39,10 +42,12 @@ impl State {
     }
 
     fn process(&mut self, message: Message) {
-        // TODO: create a match expression to process the different message
-        // variants
-        // Remember: When passing a tuple as a function argument, you'll need
-        // extra parentheses: fn function((t, u, p, l, e))
+        match message {
+            Message::ChangeColor(red, green, blue) =>  self.change_color((red, green, blue)),
+            Message::Move(point) => self.move_position(point),
+            Message::Echo(message) => self.echo(message),
+            Message::Quit => self.quit()
+        }
     }
 }
 
--- exercises/modules/modules3.rs
+++ exercises/modules/modules3.rs
@@ -1,17 +1,10 @@
 // modules3.rs
-//
-// You can use the 'use' keyword to bring module paths from modules from
-// anywhere and especially from the Rust standard library into your scope. Bring
-// SystemTime and UNIX_EPOCH from the std::time module. Bonus style points if
-// you can do it with one line!
-//
-// Execute `rustlings hint modules3` or use the `hint` watch subcommand for a
-// hint.
-
-// I AM NOT DONE
-
-// TODO: Complete this use statement
-use ???
+// You can use the 'use' keyword to bring module paths from modules from anywhere
+// and especially from the Rust standard library into your scope.
+// Bring SystemTime and UNIX_EPOCH
+// from the std::time module. Bonus style points if you can do it with one line!
+// Execute `rustlings hint modules3` or use the `hint` watch subcommand for a hint.
+use std::time::*;
 
 fn main() {
     match SystemTime::now().duration_since(UNIX_EPOCH) {
